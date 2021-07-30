ccd1c032b28cfaefbb51a1195be4012e08086b51
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-30 08:39:19 -0300

    Create Project

---
+++ api_cities/src/main/java/com/dioeverisqa/api_cities/ApiCitiesApplication.java
@@ -0,0 +1,13 @@
+package com.dioeverisqa.api_cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCitiesApplication.class, args);
+	}
+
+}
---
+++ api_cities/src/test/java/com/dioeverisqa/api_cities/ApiCitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dioeverisqa.api_cities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
