89aba78c0410ebc66297214cdf0f9f91fdb39a58
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-30 14:52:43 -0300

    Create state and your operations

--- api_cities/src/main/java/com/dioeverisqa/api_cities/model/State.java
+++ api_cities/src/main/java/com/dioeverisqa/api_cities/model/State.java
@@ -1,12 +1,15 @@
 package com.dioeverisqa.api_cities.model;
 
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-import org.hibernate.annotations.TypeDefs;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import javax.persistence.*;
-import java.util.List;
 
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "Status")
 @Table(name = "estado")
 public class State {
@@ -20,9 +23,9 @@ public class State {
     @ManyToOne
     @JoinColumn(name = "pais", referencedColumnName = "id")
     private Country country;
-    @Type(type = "jsonb")
-    @Basic(fetch = FetchType.LAZY)
-    @Column(name = "ddd", columnDefinition = "jsonb")
-    private List<Integer> ddd;
+ //   @Type(type = "jsonb")
+ //   @Basic(fetch = FetchType.LAZY)
+//    @Column(name = "ddd", columnDefinition = "jsonb")
+//    private List<Integer> ddd;
 
 }
