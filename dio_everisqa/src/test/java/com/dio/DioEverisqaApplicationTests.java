216c865dea461c75c514ab610d90bdc73799f27e
luis.eduardo <luisedu.unifal@gmail.com>
2021-07-30 08:34:00 -0300

    Create Project

---
+++ dio_everisqa/src/test/java/com/dio/DioEverisqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioEverisqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
